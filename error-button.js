26eee411bfa6798fe7a13189407d119aabf5f317
Joshua Davis <jad041@uark.edu>
2022-04-02 12:19:02 -0500

    some bad changes

---
+++ error-button.js
@@ -0,0 +1,1 @@
+document.body.innerHTML += '<marquee behavior="alternate" scrollamount="30"><marquee behavior="alternate" scrollamount="30" direction="down"><div id="button-for-error"><button id="error-button">ERROR</button></div></marquee></marquee>'
