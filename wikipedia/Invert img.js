39991b6eb534f9a768afef31bb08fb85731ff97c
TheGino06 <63830660+TheGino06@users.noreply.github.com>
2022-04-02 12:40:24 -0500

    updated image invert and headers dissapear

--- wikipedia/Invert img.js
+++ wikipedia/Invert img.js
@@ -1,9 +1,17 @@
 function invert() {
     var images = document.querySelectorAll("img");
-    for(let i = 0; i < images.length; i++)
-    {
-        images[i].style.filter = "invert(1)";
-    }
+    x = images.length
+    i = Math.floor(Math.random() * x);
+    images[i].style.filter = "invert(1)";
+}
+function revert() {
+        var images = document.querySelectorAll("img");
+        for(let j = 0; j < images.length; j++)
+        {
+            images[j].style.filter = "invert(0)";
+        }
+    
 }
 
-invert()
+	const InvertInterval = setInterval(invert, 5000);
+	const RevertInterval = setInterval(revert, 6101);
